ba6af78cf4ff85d35659020995677ff9d707a251
Jeff Steed <steed.jeff@gmail.com>
2015-10-29 16:22:36 -0500

    Boilerplate fun.

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,13 @@
 window.onload = function() {
   //alert('Hey Hey Hey');
   statusReport('Hey Hey Hey');
+
+  var username = prompt("What is your name?");
+  var mission = prompt("What is your mission?");
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + " is on a mission to " + mission + ".";
 }
 
 function statusReport(message) {
