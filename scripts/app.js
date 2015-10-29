faa54380ea80a549972c41e5837c5298e861bf25
Jeff Steed <steed.jeff@gmail.com>
2015-10-29 16:06:26 -0500

    Added JavaScript.

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+// Hey Hey Hey
+window.onload = function() {
+  //alert('Hey Hey Hey');
+  statusReport('Hey Hey Hey');
+}
+
+function statusReport(message) {
+  console.log(message);
+  return message;
+}
